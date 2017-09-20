23b2f2b5d391a143f80e7b3e27d664542958074d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-20 12:18:47 -0400

    Twin Pine Chatbot 1.0  /chat.js

--- chat.js
+++ chat.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-var iframe = document.getElementById('chat_frame');
-console.log(iframe)
-var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-var iframeContent;
+    var iframe = document.getElementById('chat_frame');
+    console.log(iframe)
+    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    console.log(iframeDocument)
+    var iframeContent;
 
-if (iframeDocument) {
-    iframeContent = iframeDocument.getElementById('stores');
-    console.log(iframeContent)
-}
+    if (iframeDocument) {
+        iframeContent = iframeDocument.getElementById('stores');
+        console.log(iframeContent)
+    }
+    
+    var iframeWindow = iframe.contentWindow;
 
-
-    var frame = $('#chat_frame').get(0).contentDocument;
-    $('#stores', frame).click(function() {
-        alert("Clicked me..!");
-    });
-
-
+    if (iframeWindow) {
+        // you can even call jQuery or other frameworks if it is loaded inside the iframe
+        iframeContent = iframeWindow.jQuery('#frameBody');
+    
+        // or
+        iframeContent = iframeWindow.$('#frameBody');
+    
+        // or even use any other global variable
+        iframeWindow.inside_iframe_variable = window.outside_iframe_variable;
+    }
 
     $("#hours").click(function() {
         console.log("HOURS!")
