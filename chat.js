690ec026d9a187c1f71f639f1a6dcb737a78ddf5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-20 12:11:44 -0400

    Twin Pine Chatbot 1.0  /chat.js

--- chat.js
+++ chat.js
@@ -6,3 +6,15 @@ var iframeContent;
 if (iframeDocument) {
     iframeContent = iframeDocument.getElementById('stores');
 }
+
+$("#hours").click(function() {
+                console.log("HOURS!")
+            });
+            
+            $("#location").click(function() {
+                console.log("LOCATION!")
+            });
+            
+            $("#contact").click(function() {
+                console.log("CONTACT!")
+            });
