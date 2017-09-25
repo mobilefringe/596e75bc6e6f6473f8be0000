9037879e3880c172f4422115d2543f707d2f1d80
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-25 10:15:38 -0400

    Twin Pine Chatbot 1.0  /chat.js

--- chat.js
+++ chat.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
-    var iframe = document.getElementById('chat_frame');
-    console.log(iframe)
-    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    var iframeWindow = iframe.contentWindow;
-    console.log(iframeWindow)
+// $(document).ready(function() {
+//     var iframe = document.getElementById('chat_frame');
+//     console.log(iframe)
+//     var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+//     var iframeWindow = iframe.contentWindow;
+//     console.log(iframeWindow)
 
-    // if (iframeWindow) {
-    //     // you can even call jQuery or other frameworks if it is loaded inside the iframe
-    //     iframeContent = iframeWindow.$('#stores');
-    //     console.log(iframeContent)
+//     // if (iframeWindow) {
+//     //     // you can even call jQuery or other frameworks if it is loaded inside the iframe
+//     //     iframeContent = iframeWindow.$('#stores');
+//     //     console.log(iframeContent)
     
-    //     // or
-    //     iframeContent = iframeWindow.$('#frameBody');
+//     //     // or
+//     //     iframeContent = iframeWindow.$('#frameBody');
     
-    //     // or even use any other global variable
-    //     iframeWindow.inside_iframe_variable = window.outside_iframe_variable;
-    // }
+//     //     // or even use any other global variable
+//     //     iframeWindow.inside_iframe_variable = window.outside_iframe_variable;
+//     // }
     
-    $('#stores', iframe).click(function() {
-                console.log("stores")
-            });
+//     $('#stores', iframe).click(function() {
+//                 console.log("stores")
+//             });
 
-    $("#hours").click(function() {
-        console.log("HOURS!")
-    });
+//     $("#hours").click(function() {
+//         console.log("HOURS!")
+//     });
     
-    $("#location").click(function() {
-        console.log("LOCATION!")
-    });
+//     $("#location").click(function() {
+//         console.log("LOCATION!")
+//     });
     
-    $("#contact").click(function() {
-        console.log("CONTACT!")
-    });
+//     $("#contact").click(function() {
+//         console.log("CONTACT!")
+//     });
     
-});
+// });
