cd6bfb2cfa6d7597433e2a5397c0a7fe6d655bbc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-23 16:08:49 -0400

    Twin Pine Chatbot 1.0  /stores.js

--- stores.js
+++ stores.js
@@ -222,22 +222,21 @@ $(document).ready(function() {
     // hide #back-top first
     $("#back-top").hide();
 
-    // fade in #back-top
-    $(function () {
-    	$(window).scroll(function () {
-    	    if ($(this).scrollTop() > 300) {
-    			$('#back-top').fadeIn();
-    		} else {
-    			$('#back-top').fadeOut();
-    		}
-    	});
-    
-    	// scroll body to 0px on click
-    	$('#back-top img').click(function () {
-    		$('body,html').animate({
-    			scrollTop: 0
-    		}, 800);
-    		return false;
-    	});
-    });
+// fade in #back-top
+$(function () {
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 300) {
+			$('#back-top').fadeIn();
+		} else {
+			$('#back-top').fadeOut();
+		}
+	});
+
+	// scroll body to 0px on click
+	$('#back-top img').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
 });
